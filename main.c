5fcd78d53fb2f74e42ec02f231cf5648b1b76c12
abdochaoubi <abdochaoubi2000@gmail.com>
2023-01-21 16:16:25 +0100

    s

--- main.c
+++ main.c
@@ -1,9 +1,28 @@
 
+#include<stdio.h>
+#include<stdlib.h>
 
 
+typedef struct s_list
+{
+    int id;
+}					t_philo;
 
 
+int main(int ac,char **av)
+{
+    if(ac!=6 && ac != 5)
+        return 0;
 
+    t_philo *philos;
+    philos = malloc(sizeof(t_philo)*10);
 
-
-
+    for(int i=0;i<5;i++)
+    {
+        philos[i].id=i;
+    }    
+    for(int i=0;i<5;i++)
+    {
+        printf("%d\n",philos[i].id);
+    }  
+}
