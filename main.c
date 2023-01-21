e365e7df1e598aa6485f9c6d3c4f8794167c0462
abdochaoubi <abdochaoubi2000@gmail.com>
2023-01-21 16:35:16 +0100

    modified:   a.out

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 
 #include<stdio.h>
 #include<stdlib.h>
-
-
-typedef struct s_list
+#include <unistd.h>
+#include <sys/time.h>
+typedef struct s_philo
 {
     int id;
 }					t_philo;
+
+typedef struct s_data
+{
+    int time_to_die;
+    int time_to_eat;
+    int time_to_sleep;
+    int what_enough ;
+    long time_start ;
+    t_philo *philos;
+}					t_data;
+
+
+long gettime(void)
+{
+        struct timeval tv;
+    gettimeofday(&tv,NULL);
+     printf("MILISeconds since 1/1/1970: %lu\n",tv.tv_sec*1000+tv.tv_usec/1000);
+return 0;
+
+}
+
 
 
 int main(int ac,char **av)
@@ -14,15 +35,17 @@ int main(int ac,char **av)
     if(ac!=6 && ac != 5)
         return 0;
 
-    t_philo *philos;
-    philos = malloc(sizeof(t_philo)*10);
+    t_data data;
+    data.philos = malloc(sizeof(t_philo)*10);
 
     for(int i=0;i<5;i++)
     {
-        philos[i].id=i;
+        data.philos[i].id=i;
     }    
     for(int i=0;i<5;i++)
     {
-        printf("%d\n",philos[i].id);
+        printf("%d\n",data.philos[i].id);
+        gettime();
+        usleep(500);
     }  
 }
