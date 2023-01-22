90ee401d17e1f52eb8305b054e131ff9ba46ecbe
abdochaoubi <abdochaoubi2000@gmail.com>
2023-01-22 16:52:58 +0100

    modified:   main.c

--- main.c
+++ main.c
@@ -50,9 +50,9 @@ void	*philo_func(void *arg)
 {
 	t_philo	*philo;
 	t_data	*data;
-	int nbrofmeal;
+	int		nbrofmeal;
 
-	nbrofmeal=0;
+	nbrofmeal = 0;
 	philo = (t_philo *)arg;
 	data = (t_data *)philo->data;
 	if (philo->id % 2 != 0)
@@ -71,70 +71,73 @@ void	*philo_func(void *arg)
 		pthread_mutex_unlock(philo->left_fork);
 		pthread_mutex_unlock(philo->right_fork);
 		nbrofmeal++;
-		if(nbrofmeal==data->what_enough)
+		if (nbrofmeal == data->what_enough)
 			data->who_complt++;
 		printf("%ld %d is sleeping\n", gettime() - data->time_start, philo->id);
 		sleep_well(data->time_to_sleep);
 		printf("%ld %d is thinking\n", gettime() - data->time_start, philo->id);
-	} 
+	}
 	return (0);
 }
 
 int	main(int ac, char **av)
 {
-	if (ac != 6 && ac != 5)
-		return (0);
-
 	t_data data;
 	pthread_t thread;
+	int i;
 
+	if (ac != 6 && ac != 5)
+		return (0);
+	i = 0;
 	data.nbm_philos = atoi(av[1]);
 	data.time_to_die = atoi(av[2]);
 	data.time_to_eat = atoi(av[3]);
 	data.time_to_sleep = atoi(av[4]);
-	if(data.time_to_die<0 || data.time_to_eat<0 || data.time_to_sleep<0 || data.nbm_philos <0)
-			return 0;
+	if (data.time_to_die < 0 || data.time_to_eat < 0 || data.time_to_sleep < 0
+		|| data.nbm_philos < 0)
+		return (0);
 	if (ac == 6)
 	{
 		data.what_enough = atoi(av[5]);
-		if(data.what_enough<=0)
-			return 0; 
+		if (data.what_enough <= 0)
+			return (0);
 	}
 	data.philos = malloc(sizeof(t_philo) * data.nbm_philos);
 	data.forks = malloc(sizeof(pthread_mutexattr_t) * data.nbm_philos);
-	for (int i = 0; i < data.nbm_philos; i++)
-	{
-		pthread_mutex_init(&data.forks[i], NULL);
-	}
-
-	for (int i = 0; i < data.nbm_philos; i++)
+	while (i < data.nbm_philos)
+		pthread_mutex_init(&data.forks[i++], NULL);
+	i = 0;
+	while (i < data.nbm_philos)
 	{
 		data.philos[i].data = &data;
 		data.philos[i].id = i;
 		data.philos[i].last_meal = gettime();
 		data.philos[i].left_fork = &data.forks[i];
 		data.philos[i].right_fork = &data.forks[(i + 1) % data.nbm_philos];
+		i++;
 	}
-	
+
 	data.time_start = gettime();
-	for (int i = 0; i < data.nbm_philos; i++)
-	{
-		pthread_create(&thread, NULL, philo_func, &data.philos[i]);
-	}
+	i = 0;
+	while (i < data.nbm_philos)
+		pthread_create(&thread, NULL, philo_func, &data.philos[i++]);
 	while (1)
 	{
-		for (int i = 0; i < data.nbm_philos; i++)
+		i = 0;
+		while (i < data.nbm_philos)
 		{
 			if (gettime() - data.philos[i].last_meal > data.time_to_die)
 			{
 				printf("%ld %d died\n", gettime() - data.time_start, i);
 				exit(0);
 			}
-			if(data.who_complt==data.nbm_philos)
+			if (data.who_complt == data.nbm_philos)
 			{
-				printf("%ld  Everyone has their meals \n", gettime() - data.time_start);
+				printf("%ld  Everyone has their meals \n", gettime()
+						- data.time_start);
 				exit(0);
 			}
+			i++;
 		}
 		usleep(500);
 	}
